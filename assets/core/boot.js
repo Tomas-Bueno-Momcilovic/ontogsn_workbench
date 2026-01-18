d2418a82b72fa80c289f5748ae731da26da0175a
Tomas Bueno Momčilović <80833185+Tomas-Bueno-Momcilovic@users.noreply.github.com>
2026-01-18 14:23:40 +0100

    hotfix pane displaying

--- assets/core/boot.js
+++ assets/core/boot.js
@@ -14,11 +14,11 @@ async function boot() {
     panes.registerPane("left", "results",         () => import("@panes/table/table.js"));
     panes.registerPane("left", "doc-root",        () => import("@panes/document/document.js"));
     panes.registerPane("left", "converter-root",  () => import("@panes/converter/converter.js"));
-    panes.registerPane("left", "audio-root",      () => import("@panes/audio/audio.js"));
     panes.registerPane("left", "code-root",       () => import("@panes/code/code.js"));
     panes.registerPane("left", "terminal-root",   () => import("@panes/terminal/terminal.js"));
     panes.registerPane("left", "chat-root",       () => import("@panes/chat/chat.js"));
 
+    panes.registerPane("right", "audio-root",      () => import("@panes/audio/audio.js"));
     panes.registerPane("right", "graph-root",     () => import("@panes/graph/graph.js"), { cache: false });
     panes.registerPane("right", "layered-root",   () => import("@panes/layers/layers.js"));
     panes.registerPane("right", "model-root",     () => import("@panes/model/model.js"));
